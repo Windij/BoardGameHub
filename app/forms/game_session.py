9f7101046069707a6ac63202447d96582904ba69
WINDIJ <max.spivak@mail.ru>
2025-04-07 14:32:52 +0500

    Добавил выбор игр из БД в Forms

--- app/forms/game_session.py
+++ app/forms/game_session.py
@@ -1,11 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired
+from ..data.games import Game
 
 
 class GameSessionForm(FlaskForm):
-    game_id = IntegerField("Id игры", validators=[DataRequired()])
+    game_id = SelectField("Id игры",coerce=int, validators=[DataRequired()])
     location = StringField("Место проведения", validators=[DataRequired()])
     max_participants = IntegerField("Максимум участников", validators=[DataRequired()])
     status = SelectField("Статус",choices=[("Запланировано", "Запланировано"), ("Завершено", "Завершено")])
     submit = SubmitField("Сохранить")
+
+    def __init__(self, *args, **kwargs):
+        super(GameSessionForm, self).__init__(*args, **kwargs)
+        self.game_id.choices = [
+            (game.id, game.title) for game in Game.query.order_by(Game.title).all()
+        ]
--- app/forms/review.py
+++ app/forms/review.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, IntegerField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, NumberRange
+from ..data.games import Game
 
 
 class ReviewForm(FlaskForm):
@@ -8,3 +9,9 @@ class ReviewForm(FlaskForm):
     rating = IntegerField("Оценка (1-5)",validators=[DataRequired(),NumberRange(min=1, max=5)])
     text = TextAreaField("Текст отзыва",validators=[DataRequired()])
     submit = SubmitField("Опубликовать")
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
+        self.game_id.choices = [
+            (game.id, game.title) for game in Game.query.order_by(Game.title).all()
+        ]
